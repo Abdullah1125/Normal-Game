34be099e3c56bf849a72892d46b4a92fea222ca9
Abdullah1125 <73892782+Abdullah1125@users.noreply.github.com>
2026-03-15 12:51:04 +0300

    Mobil Input Düzeltme
    Mobil için sağ ve sol tuşlarındaki basmama hatası giderildi.

--- Assets/Codes/PlayerController.cs
+++ Assets/Codes/PlayerController.cs
@@ -8,8 +8,8 @@ public class PlayerController : MonoBehaviour
     public float airAcceleration = 25f;
 
     [Header("Zıplama Ayarları")]
-    public float firstJumpForce = 13f;  
-    public float doubleJumpForce = 10f;  
+    public float firstJumpForce = 13f;
+    public float doubleJumpForce = 10f;
     public float fallMultiplier = 10f;
     public int extraJumpsValue = 1;
     private int extraJumps;
@@ -19,7 +19,7 @@ public class PlayerController : MonoBehaviour
     public float boostSmoothness = 3f;
 
     private Rigidbody2D rb;
-    private float moveInput;
+    private float moveInput; 
     private bool isGrounded;
     private bool isHoldingJump;
 
@@ -37,22 +37,32 @@ public class PlayerController : MonoBehaviour
 
     void Update()
     {
-        float horizontalInput = Input.GetAxisRaw("Horizontal");
-        moveInput = horizontalInput;
+        
+        float keyboardInput = Input.GetAxisRaw("Horizontal");
+        if (keyboardInput != 0)
+        {
+            moveInput = keyboardInput;
+        }
+        else if (Input.GetButtonUp("Horizontal"))
+        {
+            moveInput = 0; 
+        }
 
+        
         if (Input.GetButtonDown("Jump")) StartJump();
         if (Input.GetButtonUp("Jump")) StopJump();
 
+       
         isGrounded = Physics2D.OverlapCircle(groundCheck.position, checkRadius, groundLayer);
         if (isGrounded) extraJumps = extraJumpsValue;
 
+        
         if (moveInput > 0) transform.localScale = new Vector3(1, 1, 1);
         else if (moveInput < 0) transform.localScale = new Vector3(-1, 1, 1);
     }
 
     void FixedUpdate()
     {
-       
         if (isGrounded)
         {
             rb.linearVelocity = new Vector2(moveInput * moveSpeed, rb.linearVelocity.y);
@@ -64,7 +74,6 @@ public class PlayerController : MonoBehaviour
             rb.AddForce(speedDif * airAcceleration * Vector2.right, ForceMode2D.Force);
         }
 
-        
         if (isHoldingJump && rb.linearVelocity.y > 0.1f && rb.linearVelocity.y < 3f)
         {
             float targetY = rb.linearVelocity.y + extraBoostAmount;
@@ -72,14 +81,17 @@ public class PlayerController : MonoBehaviour
             rb.linearVelocity = new Vector2(rb.linearVelocity.x, smoothY);
         }
 
-        
         if (rb.linearVelocity.y < 0)
         {
             rb.linearVelocity += Vector2.up * Physics2D.gravity.y * (fallMultiplier - 1) * Time.fixedDeltaTime;
         }
     }
 
-    public void Move(float dir) => moveInput = dir;
+  
+    public void Move(float dir)
+    {
+        moveInput = dir;
+    }
 
     public void StartJump()
     {
@@ -99,7 +111,6 @@ public class PlayerController : MonoBehaviour
 
     private void ApplyJump(float force)
     {
-       
         rb.linearVelocity = new Vector2(rb.linearVelocity.x, force);
     }
 }
